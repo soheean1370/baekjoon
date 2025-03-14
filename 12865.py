fd156fd46f1d4ce9a23db230d8d7fd05197aa717
soheean1370 <soheean1370@pusan.ac.kr>
2025-03-14 23:53:05 +0900

    dp

---
+++ 12865.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+n,k = map(int,input().split())
+
+items = []
+for _ in range(n):
+    w,v  = map(int,input().split())
+    items.append((w,v))
+    dp = [0 for _ in range(k+1)]
+for item in items:
+    w,v = item
+    for i in range(k,w-1,-1):
+        dp[i] = max(dp[i], dp[i-w]+v)
+print(dp[-1])
+

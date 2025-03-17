05e84673d6c1e956160dbc4e64d8538baca745c9
soheean1370 <soheean1370@pusan.ac.kr>
2025-03-18 00:10:56 +0900

    sequence

--- 2559.py
+++ 2559.py
@@ -2,11 +2,16 @@ import sys
 input = sys.stdin.readline
 
 n,k = map(int, input().split())
-
 temp_list = list(map(int, input().split()))
 
-result = []
-for i in range(n - k +1):
-    result.append(sum(temp_list[i:i+k]))
+# 첫 구간의 합을 먼저 구함
+window_sum = sum(temp_list[:k])
+max_sum = window_sum
 
-print(max(result))
+# 슬라이딩 윈도우 방식으로 한칸씩 이동하며 계산
+
+for i in range(1,n-k+1):
+    window_sum = window_sum - temp_list[i -1] + temp_list[i+k-1]
+    max_sum = max(max_sum, window_sum)
+
+print(max_sum)

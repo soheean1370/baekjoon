1e7feffd70d4a48899ca1a6db8f4c92e105414ef
soheean1370 <soheean1370@pusan.ac.kr>
2025-03-25 23:59:36 +0900

    greedy

--- 1931.py
+++ 1931.py
@@ -8,12 +8,12 @@ for _ in range(n):
     full_time.append(list(map(int, input().split())))
     
 
-full_time = sorted(full_time)    
+full_time.sort(key=lambda x: x[1])  # 종료 시간 기준으로 정렬 
 end = full_time[0][1] 
 cnt = 1
 
 for i in range( 1, len(full_time)):
-    if full_time[i][1] >= end:
+    if full_time[i][0] >= end:
         cnt += 1
         end = full_time[i][1] 
 

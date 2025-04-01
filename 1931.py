ab1e63170ae2abccb09741e7f4e11040926f0326
soheean1370 <soheean1370@pusan.ac.kr>
2025-04-01 23:55:30 +0900

    dp

--- 1931.py
+++ 1931.py
@@ -8,7 +8,7 @@ for _ in range(n):
     full_time.append(list(map(int, input().split())))
     
 
-full_time.sort(key=lambda x: x[1])  # 종료 시간 기준으로 정렬 
+full_time.sort(key=lambda x: (x[1], x[0]))  # 종료 시간 우선 정렬, 같으면 시작 시간 기준 정렬
 end = full_time[0][1] 
 cnt = 1
 
